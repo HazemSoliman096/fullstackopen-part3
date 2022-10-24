4a19b09e4a13329d7c29f6bba72e54703c9d1dce
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-24 11:32:21 +0200

    3.2: Phonebook backend step2.

---
+++ data.js
@@ -0,0 +1,26 @@
+const Persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+const info = `<p>Phonebook has info for ${Persons.length} people</p><p>${new Date()}</p>`
+
+module.exports =  { Persons, info }
--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 const express = require('express');
 const app = express();
 
-const Persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
+const data = require('./data');
 
-app.get('/api/persons', (req, res) => res.json(Persons));
+app.get('/api/persons', (req, res) => res.json(data.Persons));
+
+app.get('/info', (req, res) => res.send(data.info));
 
 const PORT = 3001;
 
