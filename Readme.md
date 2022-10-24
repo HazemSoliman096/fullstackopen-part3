612eab67c1d6f8a7b829015b3e09fb04d9bad314
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-24 12:18:48 +0200

    3.3: Phonebook backend step3.

--- Readme.md
+++ Readme.md
@@ -2,3 +2,5 @@
 
 - Phonebook:
   1. 3.1: Phonebook backend step1.
+  2. 3.2: Phonebook backend step2.
+  3. 3.3: Phonebook backend step3.
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const data = require('./data');
 
 app.get('/api/persons', (req, res) => res.json(data.Persons));
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = data.Persons.filter(p => p.id === id);
+  res.json(person);
+});
+
 app.get('/info', (req, res) => res.send(data.info));
 
 const PORT = 3001;
